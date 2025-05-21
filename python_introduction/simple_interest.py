47af7a87347fdac8d7389fce16e7c728b69987aa
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-21 11:34:15 +0200

    Calculated the interest rate using the simple interest formula

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  #representing $1000
+rate = 0.05 #representing 5% annual interest rate
+time = 3 #representing 3 years
+
+interest = principal * rate * time #simple interest formula
+print("The simple interest is:", interest)
+
