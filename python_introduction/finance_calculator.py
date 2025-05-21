5c38eeee264221f91d7cc22e96a93592e8144a17
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-21 13:27:28 +0200

    Updated the formatting of whole code by using f-strings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,10 @@
-income = int(input("Enter your monthly income:"))  #Prompt the user for their monthly income
-expenses = int(input("Enter your total monthly expenses:")) #Ask for their total monthly expenses
+income = int(input("Enter your monthly income: "))  # Prompt the user for their monthly income
+expenses = int(input("Enter your total monthly expenses: "))  # Ask for their total monthly expenses
+
 savings = income - expenses
-print("Your monthly savings are", "$", savings,".")
-rates = 0.05 #Assume a simple annual interest rate of 5%
-#Calculating projected savings
-projected_savings = int(savings * 12 + (savings * 12 * rates))
-print("Projected savings after one year, with interest, is:", "$",projected_savings,".")
+print(f"Your monthly savings are ${savings}.")
+
+rates = 0.05  # Assume a simple annual interest rate of 5%
+projected_savings = int(savings * 12 + (savings * 12 * rates))  # Calculating projected savings
+
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
