35c10075109819b8aad7fcb4f98b8cd18de60845
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-21 10:53:44 +0200

    Basic math operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print("Addition of", number1, "and", number2, "is", result)
+result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", result)
+result = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", result)
