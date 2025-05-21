7d408ec877262e4186e446fd0397dd1be6c5faa9
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-21 13:30:49 +0200

    Updated the code by using the variables assigned by the autochecker

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
-income = int(input("Enter your monthly income: "))  # Prompt the user for their monthly income
-expenses = int(input("Enter your total monthly expenses: "))  # Ask for their total monthly expenses
+monthly_income = int(input("Enter your monthly income: "))  # Prompt the user for their monthly income
+monthly_expenses = int(input("Enter your total monthly expenses: "))  # Ask for their total monthly expenses
 
-savings = income - expenses
-print(f"Your monthly savings are ${savings}.")
+monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are ${monthly_savings}.")
 
-rates = 0.05  # Assume a simple annual interest rate of 5%
-projected_savings = int(savings * 12 + (savings * 12 * rates))  # Calculating projected savings
+annual_interest_rate = 0.05  # Assume a simple annual interest rate of 5%
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate))  # Calculating projected savings
 
 print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
