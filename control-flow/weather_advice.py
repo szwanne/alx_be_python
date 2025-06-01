43f31d7ed66f4e1162856ba71b58fc24e438e4ec
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-01 11:30:54 +0200

    Changed the userinput variable to weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-userinput = input("What's the weather like today? (sunny/rainy/cold):")
-if userinput == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif userinput == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif userinput == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
