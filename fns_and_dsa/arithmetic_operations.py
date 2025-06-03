60518b0c7354569e39120b8db4fc5297d227db5c
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-03 20:51:53 +0200

    Added relevant information to conduct basic mumerical operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,13 @@
 def perform_operation(num1, num2, operation):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1*num2
+    elif operation == "divide":
+        if num2 != 0:
+            result = num1/num2
+        else:
+            print("Use a number > 0")
+    return result
