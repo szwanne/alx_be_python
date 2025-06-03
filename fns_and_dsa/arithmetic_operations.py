949b24e877cfdd8e0248de8ca6b667ab3378b2f0
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-03 20:59:56 +0200

    Updated the basic arithmetic calculations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         result = num1*num2
     elif operation == "divide":
-        if num2 != 0:
-            result = num1/num2
+        if num2 == 0:
+            print("num2 should be > 0")
         else:
-            print("Use a number > 0")
+            result = num1/num2
     return result
