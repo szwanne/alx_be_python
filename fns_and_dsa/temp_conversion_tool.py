849656a18bf6d74a34332376e81ab57a0bf69a07
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-05 10:57:53 +0200

    Created two function that will convert temperature from Fahrenheit to Celsius and vice-versa

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,5 +10,10 @@ def convert_to_fahrenheit(celsius):
     return CELSIUS_TO_FAHRENHEIT_FACTOR*celsius + 32
 
 
-user_input = input("Enter the temperature")
-if user_input ==
+user_input = float(input("Enter the temperature to convert: "))
+temperature_type = input(
+    "Is this temperature in Celsius or Fahrenheit? (C/F): ")
+if temperature_type == "C":
+    print(f"{user_input}°F is {convert_to_fahrenheit(user_input)}")
+elif temperature_type == "F":
+    print(f"{user_input}°C is {convert_to_celsius(user_input)}")
