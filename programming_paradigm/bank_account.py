a7ee7340cf74f34342d9553470f3c6beee7bdca8
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-15 20:25:44 +0200

    Added the class called BankAccount and its relevant methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+# Creating a class called BankAccount and initializing the account_balance
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+# Amount to be deposited into the bank
+
+    def deposit(self, amount):
+        self.account_balance >= amount
+        return self.account_balance
+# Comparing the amount to be withdraw to the amount
+
+    def withdraw(self, amount):
+        if (self.account_balance >= amount):
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+# The function will display the account balance
+
+    def display_balance(self):
+        return self.account_balance
